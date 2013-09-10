f89d1fab782704dc2f6feff2455c9932fcb1e9f6
Eric Horacek <horacek.eric@gmail.com>
2013-09-10 09:16:35 -0600

    Removes blurring from Day Column header background

--- Example/Example/MSDayColumnHeaderBackground.m
+++ Example/Example/MSDayColumnHeaderBackground.m
@@ -8,31 +8,15 @@
 
 #import "MSDayColumnHeaderBackground.h"
 
-@interface MSDayColumnHeaderBackground ()
-
-@property (nonatomic, strong) UIToolbar *toolbar;
-
-@end
-
 @implementation MSDayColumnHeaderBackground
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Easy blurring
-        self.toolbar = [[UIToolbar alloc] initWithFrame:frame];
-        self.toolbar.autoresizingMask = (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
-        self.layer.masksToBounds = YES;
-        [self addSubview:self.toolbar];
+        self.backgroundColor = [UIColor colorWithHexString:@"f7f7f7"];
     }
     return self;
-}
-
-- (void)layoutSubviews
-{
-    [super layoutSubviews];
-    self.toolbar.frame = (CGRect){CGPointZero, self.frame.size};
 }
 
 @end
